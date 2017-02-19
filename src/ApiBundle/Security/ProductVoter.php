79f2b0993cbd14926a628f382c7506cb08374f18
crthwaite <cristofollluis.rivas@e-campus.uab.cat>
2017-02-19 18:47:06 +0100

    Add product voter for security checks

---
+++ src/ApiBundle/Security/ProductVoter.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace ApiBundle\Security;
+
+use ApiBundle\Entity\Product;
+use ApiBundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class ProductVoter extends Voter
+{
+
+    const VIEW = 'view';
+    const EDIT = 'edit';
+
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute
+     * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject)
+    {
+        // if the attribute isn't one we support, return false
+        if (!in_array($attribute, array(self::VIEW, self::EDIT))) {
+            return false;
+        }
+
+        // only vote on Product objects inside this voter
+        if (!$subject instanceof Product) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            // the user must be logged in; if not, deny access
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                return $this->canView($user);
+            case self::EDIT:
+                return $this->canEdit($user);
+        }
+
+        throw new \LogicException('This code should not be reached!');
+    }
+
+    private function canView(User $user)
+    {
+        if ($this->canEdit($user)) {
+            return true;
+        }
+
+        return $user->hasRole('ROLE_USER');
+    }
+
+    private function canEdit(User $user)
+    {
+        return $user->hasRole('ROLE_ADMIN');
+    }
+}
